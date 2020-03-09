960d035e30373b1e914f0072efc9997acdd5e56e
chedielathmni <chedielathmni@gmail.com>
2020-03-09 21:04:23 +0100

    feat: show pictograms in product

--- src/Entity/Pictogramme.php
+++ src/Entity/Pictogramme.php
@@ -3,9 +3,14 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
+
+
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PictogrammeRepository")
+ * @Vich\Uploadable
  */
 class Pictogramme
 {
@@ -20,6 +25,18 @@ class Pictogramme
      * @ORM\Column(type="string", length=255)
      */
     private $code;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $image;
+
+    /**
+     * @Vich\UploadableField(mapping="pictograms_images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
 
     public function getId(): ?int
     {
@@ -36,5 +53,38 @@ class Pictogramme
         $this->code = $code;
 
         return $this;
+    }
+
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function __toString()
+    {
+        return $this->code;
     }
 }
