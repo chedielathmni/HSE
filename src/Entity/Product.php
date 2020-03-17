355579e290341944e458abeefa222a35ace65e22
chedielathmni <chedielathmni@gmail.com>
2020-03-17 01:27:55 +0100

    feat: added file upload for products

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -5,9 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ * @Vich\Uploadable
  */
 class Product
 {
@@ -68,12 +71,31 @@ class Product
      */
     private $protection;
 
+
+
+    /**
+     * @Vich\UploadableField(mapping="fiches", fileNameProperty="fiche")
+     * @var File
+     */
+    private $ficheFile;
+
+    /**
+     * @ORM\Column(type="datetime", nullable = true)
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $fiche;
+
     public function __construct()
     {
         $this->conseil = new ArrayCollection();
         $this->mentionDangers = new ArrayCollection();
         $this->pictogramme = new ArrayCollection();
         $this->protection = new ArrayCollection();
+        $this->updatedAt = new \DateTime('now');
     }
 
     public function getId(): ?int
@@ -263,6 +285,57 @@ class Product
         if ($this->protection->contains($protection)) {
             $this->protection->removeElement($protection);
         }
+
+        return $this;
+    }
+
+    public function setFiche($fiche)
+    {
+        $this->fiche = $fiche;
+    }
+
+    public function getFiche()
+    {
+        return $this->fiche;
+    }
+
+
+    public function getFicheFile()
+    {
+        return $this->ficheFile;
+    }
+
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|UploadedFile|null $ficheFile
+     */
+    public function setFicheFile(File $fiche = null)
+    {
+        $this->ficheFile = $fiche;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($fiche) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
