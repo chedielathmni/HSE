e2ae65c2b762d0e45403c31dd42fde83076f94f2
chedielathmni <chedielathmni@gmail.com>
2020-03-19 06:41:58 +0100

    feat: Group Permission wokring

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -25,32 +25,38 @@ class GroupController extends EasyAdminController
 
         if ( in_array($change,['canManageUsers', 'canManageProducts', 'canManageGroups', 'canManageStock'])) {
 
-            switch($change){
-                case 'canManageUsers':
-                    $role = 'ROLE_USER';
-                    break;
-                case 'canManageProducts':
-                    $role = 'ROLE_ADMIN';
-                    break;
-                case 'canManageGroups':
-                    $role = 'ROLE_MOD_S';
-                    break;
-                case 'canManageStock':
-                    $role = 'ROLE_GES_STOCK';
-                    break;
-            }
+            $role = $this->getRole($change);
 
         foreach($members as $member) {
-            if ($request->query->get('newValue') == true) {
-            $member->setRoles(array_merge($member->getRoles(),[$role]));
-            $entity->setDepartmentName('true');
+            if ($value == 'true') {
+                $member->setRoles(array_merge($member->getRoles(),[$role]));
             }
-            if ($request->query->get('newValue') == false) {
-            $member->setRoles(array_diff($member->getRoles(),[$role]));
-            $entity->setDepartmentName('false');
-        }
+                if ($value == 'false') {
+                $member->setRoles(array_diff($member->getRoles(),[$role]));
+            }
         }
     }
         parent::persistEntity($entity);
+    }
+    
+
+
+    
+    private function getRole($change) {
+        switch($change){
+            case 'canManageUsers':
+                $role = 'ROLE_USER';
+            break;
+            case 'canManageProducts':
+                $role = 'ROLE_ADMIN';
+            break;
+            case 'canManageGroups':
+                $role = 'ROLE_MOD_S';
+            break;
+            case 'canManageStock':
+                $role = 'ROLE_GES_STOCK';
+            break;
+        }
+        return $role;
     }
 }
