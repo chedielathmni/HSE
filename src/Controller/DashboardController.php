eee3c6597761642048dd6998fe5e8365b979986f
chedielathmni <chedielathmni@gmail.com>
2020-06-09 11:51:45 +0100

    updated dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -16,4 +16,33 @@ class DashboardController extends AbstractController
             'controller_name' => 'DashboardController',
         ]);
     }
+
+
+    /**
+     * @Route("/dashboard/calender", name="calender")
+     */
+    public function calender() {
+        return $this->render('dashboard/calender.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+
+        /**
+     * @Route("/dashboard/zones", name="zones")
+     */
+    public function zones() {
+        return $this->render('dashboard/zones.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+
+
+        /**
+     * @Route("/dashboard/reports", name="reports")
+     */
+    public function reports() {
+        return $this->render('dashboard/reports.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
 }
